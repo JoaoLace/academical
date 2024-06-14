a2001e1bd73e5db1fc45099519d3574968a8c594
JoaoLace <joaopaulolacerdaleao@gmail.com>
2024-06-13 21:44:40 -0300

    Testing the Lexer

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-
+#include "include/lexer.h"
 int main(int agrc, char *agrv[])
 {
-    printf("hello");
+    lexer_T *lexer = init_lexer(
+        "var name = \" Joao \";\n"
+        "print(name);");
+
+    token_T *token = (void *)0;
+
+    while ((token = lexer_get_next_token(lexer)) != (void *)0)
+    {
+        printf("TOKEN(%d, %s )", token->type, token->value);
+        printf("\n");
+    }
 
     return 0;
 }

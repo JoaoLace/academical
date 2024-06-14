135bc4a43bd1e6b80bb624757af4a314474ab0b0
JoaoLace <joaopaulolacerdaleao@gmail.com>
2024-06-14 11:42:03 -0300

    Added token for coma and end of file

--- src/lexer.c
+++ src/lexer.c
@@ -55,11 +55,14 @@ token_T *lexer_get_next_token(lexer_T *lexer)
         case ')':
             return lexer_advance_with_token(lexer, init_token(TOKEN_RPAREN, lexer_get_current_char_as_string(lexer)));
             break;
+        case ',':
+            return lexer_advance_with_token(lexer, init_token(TOKEN_COMA, lexer_get_current_char_as_string(lexer)));
+            break;
         default:
             break;
         }
     }
-    return (void *)0;
+    return init_token(TOKEN_EDF, '\0');
 }
 
 token_T *lexer_collect_string(lexer_T *lexer)
